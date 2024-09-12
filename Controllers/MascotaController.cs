3bedda2f2d8118e1c6c7b77312d29829c8ce5fbd
Adriancas28 <castroadrian1228@gmail.com>
2024-09-12 15:09:51 -0500

    crud finales

--- Controllers/MascotaController.cs
+++ Controllers/MascotaController.cs
@@ -14,7 +14,7 @@ namespace CRUD.Controllers
   
      public class MascotaController : Controller
     {
-        private readonly ILogger<MascotaController> _logger;
+       private readonly ILogger<MascotaController> _logger;
         private readonly ApplicationDbContext _context;
 
         public MascotaController(ILogger<MascotaController> logger, ApplicationDbContext context)
@@ -37,24 +37,65 @@ namespace CRUD.Controllers
         }
 
         [HttpPost]
-        public IActionResult Registrar(MascotaViewModel viewModel)
+        public IActionResult Insertar(MascotaViewModel viewModel)
         {
             var fechaNacimiento = viewModel.FormMascota.FechaNacimiento.ToUniversalTime();
-            var mascota = new Mascota
+            if (viewModel.FormMascota.Id == 0)
             {
-                Nombre = viewModel.FormMascota.Nombre,
-                Raza = viewModel.FormMascota.Raza,
-                Color = viewModel.FormMascota.Color,
-                FechaNacimiento = fechaNacimiento
-            };
+                var mascota = new Mascota
+                {
+                    Nombre = viewModel.FormMascota.Nombre,
+                    Raza = viewModel.FormMascota.Raza,
+                    Color = viewModel.FormMascota.Color,
+                    FechaNacimiento = fechaNacimiento
+                };
+                _context.DBMasc.Add(mascota);
+                ViewData["Message"] = "Mascota Insertada con éxito";
+            }
+            else
+            {
+                var mascotaExistente = _context.DBMasc.Find(viewModel.FormMascota.Id);
+                if (mascotaExistente != null)
+                {
+                    mascotaExistente.Nombre = viewModel.FormMascota.Nombre;
+                    mascotaExistente.Raza = viewModel.FormMascota.Raza;
+                    mascotaExistente.Color = viewModel.FormMascota.Color;
+                    mascotaExistente.FechaNacimiento = fechaNacimiento;
+                    ViewData["Message"] = "Mascota Actualizada con éxito";
+                }
+            }
+            _context.SaveChanges();
+            viewModel.ListMascota = _context.DBMasc.ToList();
 
-            _context.DBMasc.Add(mascota);
+            return View("Index", viewModel);
+        }
+
+        public IActionResult Eliminar(long id)
+        {
+            var mascota = _context.DBMasc.Find(id);
+            _context.DBMasc.Remove(mascota);
             _context.SaveChanges();
 
-            ViewData["Message"] = "Mascota Registrada";
+            ViewData["Message"] = "Mascota Eliminada con éxito";
 
             var misMascotas = _context.DBMasc.ToList();
-            viewModel.ListMascota = misMascotas;
+            var viewModel = new MascotaViewModel
+            {
+                FormMascota = new Mascota(),
+                ListMascota = misMascotas
+            };
+
+            return View("Index", viewModel);
+        }
+
+        public IActionResult Editar(long id)
+        {
+            var mascota = _context.DBMasc.Find(id);
+            var viewModel = new MascotaViewModel
+            {
+                FormMascota = mascota,
+                ListMascota = _context.DBMasc.ToList()
+            };
 
             return View("Index", viewModel);
         }
